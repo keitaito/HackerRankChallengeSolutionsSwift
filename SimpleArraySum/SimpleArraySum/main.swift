e6e67f09aecc7b53d0e7ce358c13c5c40e9f4edd
Keita Ito <keitaitok@gmail.com>
2017-01-19 01:10:22 -0800

    Add solution

--- SimpleArraySum/SimpleArraySum/main.swift
+++ SimpleArraySum/SimpleArraySum/main.swift
@@ -6,7 +6,10 @@
 //  Copyright © 2017 Keita Ito. All rights reserved.
 //
 
-import Foundation
-
-print("Hello, World!")
-
+// input: an Integer. the size of the array.
+let n = Int(readLine()!)!
+// input: an array of Integer.
+let numbers = readLine()!.characters.split(separator: " ").map { Int(String($0))! }
+// Sum up with reduce function.
+let sum = numbers.reduce(0, +)
+print(sum)
