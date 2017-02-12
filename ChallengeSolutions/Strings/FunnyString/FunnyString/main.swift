681aca021e2f446b534bd6e89db59c2566081ca5
Keita Ito <keitaitok@gmail.com>
2017-02-11 23:31:12 -0800

    Add FunnyString

---
+++ ChallengeSolutions/Strings/FunnyString/FunnyString/main.swift
@@ -0,0 +1,20 @@
+//
+//  main.swift
+//  FunnyString
+//
+//  Created by Keita Ito on 2/11/17.
+//  Copyright © 2017 Keita Ito. All rights reserved.
+//
+
+import Foundation
+
+let t = readLine()!
+
+
+let result = true
+
+if result {
+    print("Funny")
+} else {
+    print("Not Funny")
+}
