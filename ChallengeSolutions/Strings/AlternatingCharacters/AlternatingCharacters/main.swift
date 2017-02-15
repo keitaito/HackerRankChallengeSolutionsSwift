e72918b9093ae0dfaf4cc399d7de2dbde38d4fdc
Keita Ito <keitaitok@gmail.com>
2017-02-14 23:37:25 -0800

    Add AlternatingCharacters

---
+++ ChallengeSolutions/Strings/AlternatingCharacters/AlternatingCharacters/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  AlternatingCharacters
+//
+//  Created by Keita Ito on 2/14/17.
+//  Copyright © 2017 Keita Ito. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
